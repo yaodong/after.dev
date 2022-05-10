3a739378bddbff5e4dd130366b8ea3cfdfed6902
Yaodong Zhao <git@yaodong.dev>
2022-05-10 00:33:14 -0700

    add plausible

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,6 +29,11 @@ const Layout = ({ location, title, children }) => {
         {` `}
         <a href="https://www.gatsbyjs.com">Gatsby</a>
       </footer>
+      <script
+        defer
+        data-domain="yaodong.dev"
+        src="https://plausible.io/js/plausible.js"
+      ></script>
     </div>
   )
 }
